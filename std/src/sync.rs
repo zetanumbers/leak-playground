a83824fe90978fd5b4793faf25d757d0be40c34a
Daria Sukhonina <dariasukhonina@gmail.com>
2024-02-02 13:52:39 +0300

    add sync channels replacements

--- std/src/sync.rs
+++ std/src/sync.rs
@@ -5,8 +5,28 @@ mod arc;
 pub mod mpsc {
     use std::sync::mpsc;
 
+    use crate::marker::Leak;
+
     pub fn rendezvous_channel<T>() -> (mpsc::SyncSender<T>, mpsc::Receiver<T>) {
         mpsc::sync_channel(0)
+    }
+
+    pub fn sync_channel<T: Leak>(bound: usize) -> (mpsc::SyncSender<T>, mpsc::Receiver<T>) {
+        mpsc::sync_channel(bound)
+    }
+
+    pub fn channel<T: Leak>() -> (mpsc::Sender<T>, mpsc::Receiver<T>) {
+        mpsc::channel()
+    }
+
+    pub unsafe fn sync_channel_unchecked<T>(
+        bound: usize,
+    ) -> (mpsc::SyncSender<T>, mpsc::Receiver<T>) {
+        mpsc::sync_channel(bound)
+    }
+
+    pub unsafe fn channel_unchecked<T>() -> (mpsc::Sender<T>, mpsc::Receiver<T>) {
+        mpsc::channel()
     }
 }
 
